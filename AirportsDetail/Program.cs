f3037af8935ff7a7099746d1203d9ebd8cd2a341
username <useremail@cognizant.com>
2023-10-13 10:51:45 +0530

    Add project files.

---
+++ AirportsDetail/Program.cs
@@ -0,0 +1,52 @@
+using CarRental.context;
+using CarRental.Repository;
+using CarRental.Repository.DAL;
+using Microsoft.EntityFrameworkCore;
+
+namespace AirportsDetail
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var string1= builder.Configuration.GetConnectionString("default")?.ToString();
+            builder.Services.AddDbContext<DbContextclass>(options => options.UseSqlServer(string1));
+            builder.Services.AddTransient<ICarDAL, CarDAL>();
+            builder.Services.AddTransient<ICustomerDAL, CustomerDAL>();
+            builder.Services.AddTransient<IRentDAL, RentDAL>();
+           builder.Services.AddSingleton<ExceptionHandler>();
+            builder.Services.AddMvcCore();
+            builder.Services.AddDistributedMemoryCache();
+            builder.Services.AddSession();
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+           
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseSession();
+            app.UseMiddleware<ExceptionHandler>();
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+            app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
